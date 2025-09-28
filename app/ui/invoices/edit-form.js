98b4325023edaec80153e3d490f7ab9edfa857f8
M FIKRI HAIKAL AYATULLAH <139601477+MFikriHaikalAyatullah12@users.noreply.github.com>
2025-09-28 07:37:18 +0000

    selesai chapter 12

--- app/ui/invoices/edit-form.js
+++ app/ui/invoices/edit-form.js
@@ -1,5 +1,3 @@
-'use client';
-
 import {
   CheckIcon,
   ClockIcon,
@@ -8,10 +6,13 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({ invoice, customers }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="p-4 rounded-md bg-gray-50 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
